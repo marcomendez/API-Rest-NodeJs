98950349ab5204bd35887ec72c44ab4958a9c46c
marcomendez <marco.mendez@communitybrands.com>
2019-10-04 19:02:34 -0400

    api done

--- backend/controllers/project.js
+++ backend/controllers/project.js
@@ -87,20 +87,57 @@ var controller = {
         });
     },
 
-    uploadImage: function(req,res){
-        var projectId = res.params.id;
-        var fileName = 'None...';
+    uploadImage: function(req, res){
+		var projectId = req.params.id;
+		var fileName = 'Imagen no subida...';
 
-        if(req.files){
-            return res.status(200).send({
-                file: req.files
-            });
-        }else{
-            return res.status(200).send({
-                message: fileName
-            });
-        }
-    }
+		if(req.files){
+			var filePath = req.files.image.path;
+			var fileSplit = filePath.split('\\');
+			var fileName = fileSplit[1];
+			var extSplit = fileName.split('\.');
+			var fileExt = extSplit[1];
+
+			if(fileExt == 'png' || fileExt == 'jpg' || fileExt == 'jpeg' || fileExt == 'gif'){
+
+				Project.findByIdAndUpdate(projectId, {image: fileName}, {new: true}, (err, projectUpdated) => {
+					if(err) return res.status(500).send({message: 'La imagen no se ha subido'});
+
+					if(!projectUpdated) return res.status(404).send({message: 'El proyecto no existe y no se ha asignado la imagen'});
+
+					return res.status(200).send({
+						project: projectUpdated
+					});
+				});
+
+			}else{
+				fs.unlink(filePath, (err) => {
+					return res.status(200).send({message: 'La extensión no es válida'});
+				});
+			}
+
+		}else{
+			return res.status(200).send({
+				message: fileName
+			});
+		}
+
+	},
+
+	getImageFile: function(req, res){
+		var file = req.params.image;
+		var path_file = './uploads/'+file;
+
+		fs.exists(path_file, (exists) => {
+			if(exists){
+				return res.sendFile(path.resolve(path_file));
+			}else{
+				return res.status(200).send({
+					message: "No existe la imagen..."
+				});
+			}
+		});
+	}
 };
 
 module.exports = controller;
